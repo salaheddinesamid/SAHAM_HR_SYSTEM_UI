3f19079bcda97c489778b442af44b6da8aef3b9a
salaheddinesamid <143968052+salaheddinesamid@users.noreply.github.com>
2025-12-03 09:15:17 +0100

    update

--- src/features/leaves/components/AbsenceRequestForm.jsx
+++ src/features/leaves/components/AbsenceRequestForm.jsx
@@ -148,14 +148,9 @@ export const AbsenceRequestForm = ({user})=>{
                 </Alert>
             </Snackbar>
             {loading && (
-                    <div
-                    style={{
-                        position: "absolute",
-                        top: 0,
-                        left: 0,
-                        width: "100%",
-                        height: "100%",
-                        backgroundColor: "rgba(255,255,255,0.7)",
+                <div
+                style={{
+                    position: "absolute", top: 0, left: 0, width: "100%", height: "100%", backgroundColor: "rgba(255,255,255,0.7)",
                         display: "flex",
                         justifyContent: "center",
                         alignItems: "center",
