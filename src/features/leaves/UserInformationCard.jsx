c31446f9b2dce8f26c5082ff901600ade98bab0f
salaheddinesamid <143968052+salaheddinesamid@users.noreply.github.com>
2025-12-08 15:27:25 +0100

    update

--- src/features/leaves/UserInformationCard.jsx
+++ src/features/leaves/UserInformationCard.jsx
@@ -29,6 +29,41 @@ export const UserInformationCard = ({exception,email}) => {
     return <p>Loading user info...</p>;
   }
 
+  const EmployeeBalance = ({balanceDetails})=>{
+    return(
+        <div className="row mt-3">
+            <div className="row">
+                <div className="col">
+                    <p>Solde {balanceDetails?.year} : <b>{balanceDetails?.currentBalance} Jour (s)</b></p>
+                </div>
+                
+                <div className="col">
+                    <p>Droit annuel : <b>{balanceDetails?.annualBalance} Jours</b></p>
+                </div>
+                
+                <div className="col">
+                    <p>Jours Cumulés : <b>{balanceDetails?.accumulatedBalance} Jour (s)</b></p>
+                </div>
+                
+                <div className="col">
+                    <p>Pris : <b>{balanceDetails?.usedBalance} Jour (s)</b></p>
+                </div>
+                
+                <div className="col">
+                    <p>Reliquat : <b>{balanceDetails?.reminderBalance} Jour (s)</b></p>
+                </div>
+            </div>
+            <div className="row">
+                <p><b>NB:</b> - Vous pouvez annuler votre demande de congé tant qu’elle est toujours en attente chez le manager.<p style={{
+                    fontSize : "12px"
+                }}> </p></p>
+                
+                <p style={{fontSize : "12px",marginLeft : "26px",marginTop : "-10px"}}>- Une fois votre demande de congé validée, toute demande d’annulation doit être adressée à l’équipe RH.</p>
+            </div>
+        </div>
+    )
+  }
+
   return (
     <div className="user-information-card card shadow p-3 rounded-3">
       <div className="card-body">
@@ -58,39 +93,8 @@ export const UserInformationCard = ({exception,email}) => {
         <div className="row">
         </div>
         {
-            exception !== "Without solde" ? 
-            <div className="row mt-3">
-                <div className="col">
-                    <p>Solde {user?.balanceDetails?.year} : <b>{user?.balanceDetails?.currentBalance} Jour (s)</b></p>
-                </div>
-                
-                <div className="col">
-                    <p>Droit annuel : <b>{user?.balanceDetails?.annualBalance} Jours</b></p>
-                </div>
-                
-                <div className="col">
-                    <p>Jours Cumulés : <b>{user?.balanceDetails?.accumulatedBalance} Jour (s)</b></p>
-                </div>
-                
-                <div className="col">
-                    <p>Pris : <b>{user?.balanceDetails?.usedBalance} Jour (s)</b></p>
-                </div>
-                <div className="col">
-                    <p>Reliquat : <b>{user?.balanceDetails?.reminderBalance} Jour (s)</b></p>
-                </div>
-            </div> : <></>
+            exception !== "Without solde" ? <EmployeeBalance balanceDetails={user?.balanceDetails}/>: <></>
         }
-      </div>
-      <div className="row">
-        <p><b>NB:</b> - Vous pouvez annuler votre demande de congé tant qu’elle est toujours en attente chez le manager.<p style={{
-          fontSize : "12px"
-        }}> </p></p>
-        
-        <p style={{
-          fontSize : "12px",
-          marginLeft : "26px",
-          marginTop : "-10px"
-        }}>- Une fois votre demande de congé validée, toute demande d’annulation doit être adressée à l’équipe RH.</p>
       </div>
     </div>
   );
