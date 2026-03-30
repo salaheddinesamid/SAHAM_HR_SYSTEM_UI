509a702ff77c27fe726ac5ad6b51885f47bb70eb
salaheddinesamid <143968052+salaheddinesamid@users.noreply.github.com>
2026-03-30 11:06:31 +0100

    update

--- src/features/dashboard/components/ExpenseAnalytics.jsx
+++ src/features/dashboard/components/ExpenseAnalytics.jsx
@@ -4,7 +4,21 @@ import { useEffect, useState } from "react";
 import { getExpenseAnalyticsOverview } from "../../../services/AnalyticsService";
 import BarsDataset from "./BarDataset";
 import { generateYears } from "../../payrolls/utils/YearsGeneratror";
-
+const departments = [
+    { id : 1, label : "Département financier", value : "FINANCE_DEPARTMENT"},
+    { id : 2, label : "Département juridique", value : "LEGAL_DEPARTMENT" },
+    { id : 3, label : "Département informatique", value : "IT"},
+    { id : 4, label : "Département ressources humaines", value : "HUMAN_RESOURCES_DEPARTMENT" },
+    { id : 5, label : "Opérations", value : "OPERATIONS"},
+    { id : 6, label : "Asset management", value : "ASSET_MANAGEMENT" },
+    { id : 7, label : "Cabinet du DG", value : "CEO_OFFICE" },
+    { id : 8, label : "Surveillance bancaire", value : "BANKING_SUPERVISION" },
+]
+const entities  = [
+    { id : 1, label : "SAHAM Horizon", value : "SAHAM_HORIZON"},
+    { id : 2, label : "SAHAM Finances", value : "SAHAM_FINANCES"},
+    { id : 3, label : "SAHAM Foundation", value : "SAHAM_FOUNDATION"}
+]
 export const ExpenseAnalytics = () =>{
 
     const years = generateYears(2010, new Date().getFullYear);
@@ -12,6 +26,8 @@ export const ExpenseAnalytics = () =>{
     const [dataSet, setDataSet] = useState([]);
     const [overviewData, setOverviewData] = useState([]);
     const [loading, setLoading] = useState(false);
+    const [currentDepartment, setCurrentDepartment] = useState("ALL");
+    const [currentEntity, setCurrentEntity] = useState("ALL");
 
     const fetchData = async(year) =>{
         try{
@@ -58,11 +74,34 @@ export const ExpenseAnalytics = () =>{
         <p>Aucune donnée de dépense disponible</p>
       )}
       <Box
-        display="flex"
+        display="inline"
         gridTemplateColumns={{ xs: "1fr", md: "320px 1fr" }}
         gap={4}
         alignItems="center"
       >
+        <Box sx={{
+                      display : "flex",
+                      justifyContent : "space-between"
+                    }}>
+                      <select name="" id="" value={selectedYear} onChange={(e)=> setSelectedYear(e.target.value)}>
+                            <option value="">Filtrer par département</option>
+                            {departments.map((e)=>(
+                                <option value={e.value} key={e.id}>{e.label}</option>
+                            ))}
+                        </select>
+                      <select name="" id="" value={currentDepartment} onChange={(e)=> setCurrentDepartment(e.target.value)}>
+                            <option value="">Filtrer par département</option>
+                            {departments.map((e)=>(
+                                <option value={e.value} key={e.id}>{e.label}</option>
+                            ))}
+                        </select>
+                        <select name="" id="" value={currentEntity} onChange={(e)=> setCurrentEntity(e.target.value)}>
+                            <option value="">Filtrer par entité</option>
+                            {entities.map((e)=>(
+                                <option value={e.value} key={e.id}>{e.label}</option>
+                            ))}
+                      </select>
+                    </Box>
         <BarsDataset data={dataSet}/>
 
         
